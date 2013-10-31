935dadb205381781d3f362f826e446496e084809
Connor McArthur <cmcarthur@rjmetrics.com>
2013-10-31 19:24:59 -0400

    working scheduler

--- project.clj
+++ project.clj
@@ -4,5 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.novemberain/monger "1.5.0"]]
+                 [com.novemberain/monger "1.5.0"]
+                 [com.amazonaws/aws-java-sdk "1.6.3"]
+                 [jarohen/chime "0.1.2"]]
   :main cmcarthur.mongo-cloudwatch.core)
--- src/cmcarthur/mongo_cloudwatch/core.clj
+++ src/cmcarthur/mongo_cloudwatch/core.clj
@@ -1,29 +1,48 @@
 (ns cmcarthur.mongo-cloudwatch.core
-  (:require [monger.core :as mongo]))
+  (:require [monger.core :as mongo]
+            [chime]
+            [clj-time.periodic :as periodic]
+            [clj-time.core :as time]))
+
+;; mongo
 
 (def last-value (atom nil))
 
 (defn get-mongo-stats
+  "Given that a working mongo connection is already established, this function will
+  pull stats from the mongo server and destructure them into something we care about."
   []
-  (let [{{inserts "insert" queries "query" updates "update"} "opcounters" {{total "total"} "currentQueue"} "globalLock"} (mongo/command {:serverStatus 1})]
-    {:lock-queue-size total :query-totals {:inserts inserts :queries queries :updates updates}}))
+  (let [{host "host" {inserts "insert" queries "query" updates "update"} "opcounters" {{total "total"} "currentQueue"} "globalLock"} (mongo/command {:serverStatus 1})]
+    {:host host :lock-queue-size total :query-totals {:inserts inserts :queries queries :updates updates}}))
 
 (defn calculate-new-mongo-values
+  "Given that a working mongo connection is already established, this function will
+  pull some basic stats from the server: lock queue size, and number of queries run
+  since the last run of this function.
+
+  Since it takes a diff between calls, it's highly recommended that you call this
+  function at a known interval."
   []
   (let [mongo-stats (get-mongo-stats)
         new-values (:query-totals mongo-stats)
         old-values (deref last-value)]
     (if (nil? old-values)
         (do (reset! last-value new-values) nil)
-        {:lock-queue-size (:lock-queue-size mongo-stats) :query-totals (merge-with - new-values old-values)})))
+        {:host (:host mongo-stats)
+         :lock-queue-size (:lock-queue-size mongo-stats)
+         :query-totals (merge-with - new-values old-values)})))
+
+;; cloudwatch
 
 (defn create-push-metric-request
+  "Wrapper for the PutMetricDataRequest class provided by the AWS Cloudwatch SDK."
   [namespace metric-data]
   (-> (com.amazonaws.services.cloudwatch.model.PutMetricDataRequest.)
       (.withNamespace namespace)
       (.withMetricData metric-data)))
 
 (defn create-metric-datum
+  "Wrapper for the MetricDatum class provided by the AWS Cloudwatch SDK."
   [dimensions name unit value]
   (-> (com.amazonaws.services.cloudwatch.model.MetricDatum.)
       (.withDimensions dimensions)
@@ -32,13 +51,38 @@
       (.withValue value)))
 
 (defn create-dimension
+  "Wrapper for the Dimension class provided by the AWS Cloudwatch SDK."
   [name value]
   (-> (com.amazonaws.services.cloudwatch.model.Dimension.)
       (.withName name)
       (.withValue value)))
 
 (defn push-metric
+  "Given a namespace and a metric data collection, this function will push
+  the metric data to Cloudwatch."
   [namespace metric-data]
   (let [push-request (create-push-metric-request namespace metric-data)
         client (com.amazonaws.services.cloudwatch.AmazonCloudWatchClient.)]
     (.putMetricData client push-request)))
+
+;; link
+
+(defn create-cloudwatch-metrics-from-mongo-stats
+  [mongo-stats]
+  (let [{lock-queue-size :lock-queue-size {inserts :inserts queries :queries updates :updates} :query-totals} mongo-stats
+        dimensions [(create-dimension "Hostname" (mongo-stats :host))]]
+    [(create-metric-datum dimensions "Inserts" "Count" (double inserts))
+     (create-metric-datum dimensions "Queries" "Count" (double queries))
+     (create-metric-datum dimensions "Updates" "Count" (double updates))
+     (create-metric-datum dimensions "Lock Queue Size" "Count" (double lock-queue-size))]))
+
+(defn run-scheduler
+  []
+  (chime/chime-at (rest (periodic/periodic-seq (time/now)
+                                               (time/secs 15)))
+                  (fn [time]
+                    (let [to-push (calculate-new-mongo-values)]
+                      (when-not (nil? to-push)
+                        (do (println "Pushing...")
+                            (push-metric "Service/Mongo"
+                                         (create-cloudwatch-metrics-from-mongo-stats to-push))))))))
